2fd510202887f1f8bc853f24a7cadefa1b47dba9
Sajid Ali <sajid.ale1996@gmail.com>
2024-05-15 20:50:13 +0500

    Delete Unnecessary Files

--- cypress/support/Constants/Login-Page-Constants.js
+++
@@ -1,6 +0,0 @@
-export const LoginPageConstants = {
-    VerifyText: "Please login to make appointment",
-    WriteTextInComment: "Testing Cypress 123",
-    UserName: "John Doe",
-    Password: "ThisIsNotAPassword",
-}
