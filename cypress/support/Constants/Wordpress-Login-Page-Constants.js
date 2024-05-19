d9e8aee2a85d5b005e7798948aa7486588443ba0
Sajid Ali <sajid.ale1996@gmail.com>
2024-05-19 18:38:15 +0500

    New Changes for Elements & Constants Files

--- cypress/support/Constants/Wordpress-Login-Page-Constants.js
+++ cypress/support/Constants/Wordpress-Login-Page-Constants.js
@@ -1,7 +1,6 @@
 export const WordpressLoginPageConstants = {
         name: "admin",
         successMessage:  "Success! The test resulted in a successful connection.",
-        invalidLicenseKey: "MQ9LDWS7-NRCXH2IR-AYOO5LOU-K9S2RKEC4",
         invalidLicenseKeyErrorMessage: "Error! Plugin license key is invalid for this domain.",
         idAlertMessage: "Client ID is required.",
         secretAlertMessage: "Client Secret is required.",
