c88ca89f9c22d6e6841fe7f998ab556123e6412f
Sajid Ali <sajid.ale1996@gmail.com>
2024-05-19 18:04:50 +0500

    Adding Elements & Constants New Files

--- cypress/support/Constants/Wordpress-Login-Page-Constants.js
+++ cypress/support/Constants/Wordpress-Login-Page-Constants.js
@@ -1,7 +1,5 @@
 export const WordpressLoginPageConstants = {
         name: "admin",
-      //  expireLicenseKey: "MQ9LDWS7-NRCXH2IR-AYOO5LOU-K9S2RKEC",
-      //  licenseKey: "EO60N4L3-8XPDXBT8-6DLMHIJH-A6VFQEYZ",
         successMessage:  "Success! The test resulted in a successful connection.",
         invalidLicenseKey: "MQ9LDWS7-NRCXH2IR-AYOO5LOU-K9S2RKEC4",
         invalidLicenseKeyErrorMessage: "Error! Plugin license key is invalid for this domain.",
