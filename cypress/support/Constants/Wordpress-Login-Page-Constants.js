a582a4a0f8a7a9aee689b1272a56472aa35aa0a1
Sajid Ali <sajid.ale1996@gmail.com>
2024-05-19 17:59:24 +0500

    Separate Elements & Constants files

--- cypress/support/Constants/Wordpress-Login-Page-Constants.js
+++ cypress/support/Constants/Wordpress-Login-Page-Constants.js
@@ -1,7 +1,5 @@
 export const WordpressLoginPageConstants = {
         name: "admin",
-        clientID: "bc69f00c-1f49-4d1e-a8e5-3e7a920945b3",
-        clientSecret: "kZa8Q~CctaiaGc~79ouyVFk4ksOuasRvUM595c0B",
         expireLicenseKey: "MQ9LDWS7-NRCXH2IR-AYOO5LOU-K9S2RKEC",
         licenseKey: "EO60N4L3-8XPDXBT8-6DLMHIJH-A6VFQEYZ",
         successMessage:  "Success! The test resulted in a successful connection.",
